be3e638acbb2c8e25dc6d13d2227cc846a11a2f6
Adam Bates <adam@adambates.ca>
2022-03-30 12:18:52 -0400

    Added very simple footer content.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { Footer, Text } from "@mantine/core";
 
 export default () => {
   return (
-    <Footer height={60} p="md">
-      <Text>Footer</Text>
+    <Footer height={45} pt="xs" pl="xl">
+      <Text size="sm">&copy; {new Date().getFullYear()}</Text>
     </Footer>
   );
 };
